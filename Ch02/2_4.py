e5a65b728e70c6502fe56d86894cced45d9e4eb4
chlguddh23 <chlguddh23@naver.com>
2021-04-27 15:08:55 +0900

    2021/04/27 Ch02파이썬 예제

--- Ch02/2_4.py
+++ Ch02/2_4.py
@@ -8,3 +8,27 @@ print(name*3)
 
 msg = "hello world"
 print(len(msg))
+
+print(msg[0])
+print(msg[6])
+print(msg[-1])
+print(msg[-5])
+
+print(msg[0:5])
+print(msg[:5])
+print(msg[6:10])
+print(msg[6:])
+
+people = "김유신|김춘추|장보고|강감찬|이순신"
+
+p1, p2, p3, p4, p5 = people.split("|")
+
+print(p1)
+print(p2)
+print(p3)
+print(p4)
+print(p5)
+
+print("서울\n대전\n대구\n부산\n광주")
+print("서울\t대전\t대구\t부산\t광주")
+print("저는 '홍길동'입니다")
---
+++ Ch02/2_5.py
@@ -0,0 +1,10 @@
+print("hello", end= "!")
+print("python")
+
+num = int(input("숫자 입력하시오"))
+
+print(type(num))
+
+print("%d년 %d월 %d일 %s요일" % (2021, 4, 27, '화'))
+
+print("이름 : {} 나이: {}, 주소: {}".format("김유신", 23, "김해시"))
